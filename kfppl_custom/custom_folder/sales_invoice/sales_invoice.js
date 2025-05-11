c6d1f11b82ad588b186deddbf739ad786a879f01
Kamal Paryani <kamalparyani@gmail.com>
2025-05-12 00:01:39 +0530

    debugging

--- kfppl_custom/custom_folder/sales_invoice/sales_invoice.js
+++ kfppl_custom/custom_folder/sales_invoice/sales_invoice.js
@@ -1,27 +1,41 @@
+function log_payment_terms_from_first_item(frm) {
+    const firstItem = frm.doc.items && frm.doc.items[0];
+
+    if (firstItem && firstItem.item_code) {
+        frappe.db.get_doc('Item', firstItem.item_code).then(itemDoc => {
+            if (itemDoc.item_group) {
+                frappe.db.get_doc('Item Group', itemDoc.item_group).then(groupDoc => {
+                    console.log("Item Group:", itemDoc.item_group);
+                    console.log("Payment Terms Template:", groupDoc.custom_payment_terms_template);
+                }).catch(err => {
+                    console.error("Error fetching Item Group:", err);
+                });
+            }
+        }).catch(err => {
+            console.error("Error fetching Item:", err);
+        });
+    }
+}
+
 frappe.ui.form.on('Sales Invoice Item', {
     item_code: function(frm, cdt, cdn) {
         const row = locals[cdt][cdn];
         const firstItem = frm.doc.items && frm.doc.items[0];
 
-        if (firstItem && row.name === firstItem.name && row.item_code) {
+        if (firstItem && row.name === firstItem.name) {
             console.log("First item changed:", row.item_code);
-
-            frappe.db.get_doc('Item', row.item_code).then(itemDoc => {
-                console.log("Item Group:", itemDoc.item_group);
-
-                if (itemDoc.item_group) {
-                    frappe.db.get_doc('Item Group', itemDoc.item_group).then(groupDoc => {
-                        console.log("Payment Terms Template:", groupDoc.custom_payment_terms_template);
-                    }).catch(err => {
-                        console.error("Error fetching Item Group:", err);
-                    });
-                }
-            }).catch(err => {
-                console.error("Error fetching Item:", err);
-            });
+            log_payment_terms_from_first_item(frm);
         } else {
             console.log("Not the first item, skipping");
         }
     }
 });
 
+// Trigger after Delivery Note pull
+frappe.ui.form.on('Sales Invoice', {
+    refresh: function(frm) {
+        setTimeout(() => {
+            log_payment_terms_from_first_item(frm);
+        }, 500);
+    }
+});
