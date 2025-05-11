9cff60040a4d8ed38f03d396f49c4fe355919c1e
Kamal Paryani <kamalparyani@gmail.com>
2025-05-11 23:34:54 +0530

    debugging

--- kfppl_custom/custom_folder/sales_invoice/sales_invoice.js
+++ kfppl_custom/custom_folder/sales_invoice/sales_invoice.js
@@ -3,8 +3,14 @@ frappe.ui.form.on('Sales Invoice Item', {
         const row = locals[cdt][cdn];
         const firstItem = frm.doc.items && frm.doc.items[0];
 
-        if (firstItem && row.name === firstItem.name) {
+        if (firstItem && row.name === firstItem.name && row.item_code) {
             console.log("First item changed:", row.item_code);
+
+            frappe.db.get_doc('Item', row.item_code).then(itemDoc => {
+                console.log("Item Group:", itemDoc.item_group);
+            }).catch(err => {
+                console.error("Failed to fetch Item doc:", err);
+            });
         } else {
             console.log("Not the first item, skipping");
         }
