c3244df04c672a4d9972f87aa998d80501112dad
Kamal Paryani <kamalparyani@gmail.com>
2025-05-11 20:37:41 +0530

    debugging

--- kfppl_custom/custom_folder/sales_invoice/sales_invoice.js
+++ kfppl_custom/custom_folder/sales_invoice/sales_invoice.js
@@ -1,10 +1,6 @@
 frappe.ui.form.on('Sales Invoice Item', {
     item_code: function(frm, cdt, cdn) {
-        // frm: the parent form (Sales Invoice)
-        // cdt: child DocType ("Sales Invoice Item")
-        // cdn: child document name (like "b28f23e37a")
-        
-        let row = locals[cdt][cdn]; // access the current child row
-        console.log(row.item_code); // this is the changed value
+        let row = locals[cdt][cdn];
+        console.log(row.item_code);
     }
 });
