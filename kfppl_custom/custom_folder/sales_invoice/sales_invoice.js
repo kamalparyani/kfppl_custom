dbd14f6c81522ae7875177a2a24bfb453cc4e5a1
Kamal Paryani <kamalparyani@gmail.com>
2025-05-11 23:38:37 +0530

    debugging

--- kfppl_custom/custom_folder/sales_invoice/sales_invoice.js
+++ kfppl_custom/custom_folder/sales_invoice/sales_invoice.js
@@ -8,11 +8,20 @@ frappe.ui.form.on('Sales Invoice Item', {
 
             frappe.db.get_doc('Item', row.item_code).then(itemDoc => {
                 console.log("Item Group:", itemDoc.item_group);
+
+                if (itemDoc.item_group) {
+                    frappe.db.get_doc('Item Group', itemDoc.item_group).then(groupDoc => {
+                        console.log("Payment Terms Template:", groupDoc.custom_payment_terms_template);
+                    }).catch(err => {
+                        console.error("Error fetching Item Group:", err);
+                    });
+                }
             }).catch(err => {
-                console.error("Failed to fetch Item doc:", err);
+                console.error("Error fetching Item:", err);
             });
         } else {
             console.log("Not the first item, skipping");
         }
     }
 });
+
