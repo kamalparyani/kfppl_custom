3f8ecd3344dc486fb155c1dc2440e29f099e6751
Kamal Paryani <kamalparyani@gmail.com>
2025-05-11 20:36:48 +0530

    debugging

--- kfppl_custom/custom_folder/sales_invoice/sales_invoice.js
+++ kfppl_custom/custom_folder/sales_invoice/sales_invoice.js
@@ -1,18 +1,10 @@
 frappe.ui.form.on('Sales Invoice Item', {
     item_code: function(frm, cdt, cdn) {
-        let row = locals[cdt][cdn];
-
-        // Always run logic if this is the first item
-        if (row.idx === 1 && row.item_code) {
-            frappe.db.get_doc('Item', row.item_code).then(itemDoc => {
-                if (itemDoc.item_group) {
-                    frappe.db.get_doc('Item Group', itemDoc.item_group).then(groupDoc => {
-                        if (groupDoc.payment_terms_template) {
-                            frm.set_value('payment_terms_template', groupDoc.payment_terms_template);
-                        }
-                    });
-                }
-            });
-        }
+        // frm: the parent form (Sales Invoice)
+        // cdt: child DocType ("Sales Invoice Item")
+        // cdn: child document name (like "b28f23e37a")
+        
+        let row = locals[cdt][cdn]; // access the current child row
+        console.log(row.item_code); // this is the changed value
     }
 });
