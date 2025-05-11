3c491634d9fd0cca71721f3db1148410dca36ab1
Kamal Paryani <kamalparyani@gmail.com>
2025-05-12 00:59:41 +0530

    debugging

--- kfppl_custom/custom_folder/sales_invoice/sales_invoice.js
+++ kfppl_custom/custom_folder/sales_invoice/sales_invoice.js
@@ -1,4 +1,5 @@
-function set_payment_terms_from_first_item(frm) {
+// Forcefully set payment terms (used on item_code change)
+function force_set_payment_terms_template(frm) {
     const firstItem = frm.doc.items && frm.doc.items[0];
 
     if (firstItem && firstItem.item_code) {
@@ -6,23 +7,47 @@ function set_payment_terms_from_first_item(frm) {
             if (itemDoc.item_group) {
                 frappe.db.get_doc('Item Group', itemDoc.item_group).then(groupDoc => {
                     const template = groupDoc.custom_payment_terms_template;
-                    console.log("Item Group:", itemDoc.item_group);
-                    console.log("Payment Terms Template:", template);
 
                     if (template) {
+                        console.log("Force setting Payment Terms Template to:", template);
                         frm.set_value('payment_terms_template', template);
                     }
-                }).catch(err => {
-                    console.error("Error fetching Item Group:", err);
                 });
             }
-        }).catch(err => {
-            console.error("Error fetching Item:", err);
         });
     }
 }
 
-// Trigger when item_code is manually changed in the first row
+// Set only if not already set (used on form refresh)
+function safe_set_payment_terms_template(frm) {
+    if (!frm.doc.payment_terms_template && frm.doc.items && frm.doc.items.length) {
+        const firstItem = frm.doc.items[0];
+
+        if (firstItem.item_code) {
+            frappe.db.get_doc('Item', firstItem.item_code).then(itemDoc => {
+                if (itemDoc.item_group) {
+                    frappe.db.get_doc('Item Group', itemDoc.item_group).then(groupDoc => {
+                        const template = groupDoc.custom_payment_terms_template;
+
+                        if (template) {
+                            console.log("Safely setting Payment Terms Template to:", template);
+                            frm.set_value('payment_terms_template', template);
+                        }
+                    });
+                }
+            });
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
 frappe.ui.form.on('Sales Invoice Item', {
     item_code: function(frm, cdt, cdn) {
         const row = locals[cdt][cdn];
@@ -30,19 +55,25 @@ frappe.ui.form.on('Sales Invoice Item', {
 
         if (firstItem && row.name === firstItem.name) {
             console.log("First item changed:", row.item_code);
-            set_payment_terms_from_first_item(frm);
-        } else {
-            console.log("Not the first item, skipping");
+            force_set_payment_terms_template(frm);
         }
     }
 });
 
-// Trigger after pulling items from Sales Order / Delivery Note
 frappe.ui.form.on('Sales Invoice', {
     refresh: function(frm) {
         setTimeout(() => {
-            set_payment_terms_from_first_item(frm);
+            safe_set_payment_terms_template(frm);
         }, 500);
+    },
+
+    items_remove: function(frm, cdt, cdn) {
+        const removed_row = locals[cdt][cdn];
+        const was_first_row = removed_row.name === frm.doc.items[0]?.name;
+
+        if (was_first_row || frm.doc.items.length === 0) {
+            console.log("First item removed. Clearing Payment Terms Template.");
+            frm.set_value('payment_terms_template', null);
+        }
     }
 });
-
