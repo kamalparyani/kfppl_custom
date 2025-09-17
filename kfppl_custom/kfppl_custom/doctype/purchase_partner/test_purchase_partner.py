f7a28b13d790278b2f0714ace5beac9497ba48dd
Kamal Paryani <kamalparyani@gmail.com>
2025-09-17 09:58:02 +0530

    changes

---
+++ kfppl_custom/kfppl_custom/doctype/purchase_partner/test_purchase_partner.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, V12 Infotech and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPurchasePartner(FrappeTestCase):
+	pass
